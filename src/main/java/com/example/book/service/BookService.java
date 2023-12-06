85f7a31a3f9afd92e0bb6df4fa02ffc923812a67
gilgyujeong <jgg7645@naver.com>
2023-12-07 01:38:41 +0900

    feat : book repository 추가

--- src/main/java/com/example/book/service/BookService.java
+++ src/main/java/com/example/book/service/BookService.java
@@ -2,12 +2,17 @@ package com.example.book.service;
 
 import com.example.book.dto.BookRegistrationDto;
 import com.example.book.dto.ResponseStatusDto;
+import com.example.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import static com.example.book.entity.HttpStatusEnum.OK;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
     public ResponseStatusDto bookRegistration(BookRegistrationDto bookRegistrationDto) {
         return ResponseStatusDto.builder()
                 .statusCode(OK.getStatusCode())
